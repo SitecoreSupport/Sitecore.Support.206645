65e94f2f3ce7e70aaedb00b8be38b74a9714e45d
Andrey Filchenkov <22079019+AndreyFilchenkov@users.noreply.github.com>
2020-12-20 18:11:16 +0200

    Initial commit

---
+++ src/Sitecore.Support.206645/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.206645")]
+[assembly: AssemblyProduct("Sitecore.Support.206645")]
+[assembly: ComVisible(false)]
