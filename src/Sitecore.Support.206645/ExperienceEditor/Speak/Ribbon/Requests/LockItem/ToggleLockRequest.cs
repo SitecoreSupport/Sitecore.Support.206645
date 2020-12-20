a91cc072e78caa7b847c0caf6bc48beb97099af4
Andrey Filchenkov <22079019+AndreyFilchenkov@users.noreply.github.com>
2020-12-20 18:35:57 +0200

    Import source code

---
+++ src/Sitecore.Support.206645/ExperienceEditor/Speak/Ribbon/Requests/LockItem/ToggleLockRequest.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.ContentSearch;
+using Sitecore.Data.Items;
+
+namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.LockItem
+{
+    /// <summary>
+    /// The toggle lock request.
+    /// </summary>
+    public class ToggleLockRequest : Sitecore.ExperienceEditor.Speak.Ribbon.Requests.LockItem.ToggleLockRequest
+    {
+
+        /// <summary>
+        /// Refreshes the index.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        protected override void RefreshIndex(Item item)
+        {
+            using (IProviderSearchContext searchContext = ContentSearchManager.GetIndex(SearchDefaults.SitecoreIndex).CreateSearchContext())
+            {
+                searchContext.Index.Refresh(new SitecoreIndexableItem(item), IndexingOptions.ForcedIndexing);
+            }
+        }
+    }
+}
